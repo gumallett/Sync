ed5cf397c11d3f2fe6b03d7116fa276f669ed098
gumallett <gumallett@gmail.com>
2013-01-18 20:37:26 -0500

    update

--- Sync/main.c
+++ Sync/main.c
@@ -4,36 +4,59 @@
 #include "playlist.h"
 #include "sync.h"
 
-static void print_usage();
+static void print_usage(void);
+static int parse_args(int, _TCHAR**, TCHAR*, TCHAR**);
 
 int _tmain(int argc, _TCHAR* argv[]) {
-	char buf[10];
 	Playlist *list;
 	TCHAR *playlist_file = L"E:\\Documents and Settings\\All Users\\Documents\\My Music\\My Playlists\\current.m3u";	
 	List *dest_list = new_list();
+	TCHAR *c;
+	int i;
 
-	add_item(dest_list, L"K:\\MUSIC\\*");
-	add_item(dest_list, L"L:\\MUSIC\\*");
+	//add_item(dest_list, L"K:\\MUSIC\\*");
+	//add_item(dest_list, L"L:\\MUSIC\\*");
 
 	if(argc == 2 && wcsncmp(L"/?", argv[1], 2) == 0) {
 		print_usage();
 	}
 
-	list = new_playlist(playlist_file);
-
-	if(list != NULL) {
-		printf("size: %d", list->size);
+	if(argc > 2) {
+		
 	}
 
-	sync(list, dest_list);
+	//list = new_playlist(playlist_file);
+	//sync(list, dest_list);
 
 	//wait for user input
-	fgets(buf, 10, stdin);
+	getchar();
 
 	return 0;
 }
 
 static void print_usage() {
-	printf("Usage: sync -p [playlist file]");
+	printf("Usage: sync -p [playlist file] [ [dest1] [dest2] ... ]");
 	exit(0);
+}
+
+static int parse_args(int argc, _TCHAR *argv[], TCHAR *playlist_path, TCHAR *dests[]) {
+	int i, j, k = 0;
+	int err;
+
+	for(i=1; i<argc; i++) {
+		if(wcscmp(L"-p", argv[i]) == 0) {
+			j = i + 1;
+
+			if(j < argc) {
+				wcscpy_s(playlist_path, MAX_PATH, argv[j]);
+			}
+			else {
+				return -1;
+			}
+		}
+		else {
+			wcscpy_s(dests[k], 5, argv[i]);
+			k++;
+		}
+	}
 }
